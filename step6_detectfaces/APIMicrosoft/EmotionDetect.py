a4b664cca8bc5de14d80750561e1ae141c6da16d
zhangli <zhangli19890618@gmail.com>
2016-12-08 01:52:30 +0800

    use azure free 200$ now

--- step6_detectfaces/APIMicrosoft/EmotionDetect.py
+++ step6_detectfaces/APIMicrosoft/EmotionDetect.py
@@ -7,24 +7,25 @@ import json, os
 from Service import MSSEmotion
 
 if __name__ == '__main__':
-    service = MSSEmotion()
+    EMOTION_KEY = '6d219e9aaf50437d91b8529a71583652'
+    service = MSSEmotion(EMOTION_KEY = EMOTION_KEY)
     
-#     print '----start testing emotion from url'
-#     url = 'https://d.ibtimes.co.uk/en/full/1356835/number-2-u-s-president-barack-obama-second-most-admired-person-planet.jpg?w=400'
-#     data = service.detect_emotion_url(url)
-#     json_data = json.loads(data)
-#     print json_data
-#     print '----finish testing emotion from url'
-
-    print '----start testing emotion from local img'
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    img_path = os.path.join(base_dir, '1.jpg')
-    data = service.detect_emotion_img(img_path)
+    print '----start testing emotion from url'
+    url = 'https://d.ibtimes.co.uk/en/full/1356835/number-2-u-s-president-barack-obama-second-most-admired-person-planet.jpg?w=400'
+    data = service.detect_emotion_url(url)
     json_data = json.loads(data)
     print json_data
-    result = []
-    for face in json_data:
-        tmplist = sorted(face['scores'].items(), key = lambda x: x[1], reverse = True)
-        result.append((face['faceRectangle'], tmplist[0][0], face['scores']))
-    print len(result), result
-    print '----finish testing emotion from local img'
+    print '----finish testing emotion from url'
+
+#    print '----start testing emotion from local img'
+#    base_dir = os.path.dirname(os.path.abspath(__file__))
+#    img_path = os.path.join(base_dir, '1.jpg')
+#    data = service.detect_emotion_img(img_path)
+#    json_data = json.loads(data)
+#    print json_data
+#    result = []
+#    for face in json_data:
+#        tmplist = sorted(face['scores'].items(), key = lambda x: x[1], reverse = True)
+#        result.append((face['faceRectangle'], tmplist[0][0], face['scores']))
+#    print len(result), result
+#    print '----finish testing emotion from local img'
